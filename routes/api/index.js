77398ccb6944a5dcfd69432a31ee84fd887313a4
unknown <charlyoma007@gmail.com>
2017-12-09 16:22:01 +0100

    API and testing setup.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 
+/* GET home page. */
 router.get('/', (req, res, next) => {
-	res.json({ text: 'MusicList API Test' })
+  res.render('index', { title: 'MusicList Alpha' });
 });
 
 module.exports = router;
